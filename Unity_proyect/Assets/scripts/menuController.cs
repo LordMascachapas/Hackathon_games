c0d42cb5d8ce2be361b7cef54bd45c0036824ed8
LordMascachapas <alfredm3007@gmail.com>
2018-10-09 20:22:32 +0200

    mnu principal niadido

---
+++ Unity_proyect/Assets/scripts/menuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menuController : MonoBehaviour {
+
+    public void SartGame()
+    {
+        SceneManager.LoadScene("OnGame");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit :D");
+        Application.Quit();
+    }
+}
