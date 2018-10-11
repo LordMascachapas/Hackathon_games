1f40d0f773f180449bc0026734a7b3c50d538243
LordMascachapas <alfredm3007@gmail.com>
2018-10-11 11:28:29 +0200

    Aro con movimiento simple para ganar puntos

---
+++ Unity_proyect/Assets/scripts/gravityRing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravityRing : MonoBehaviour {
+    public float speedTranslateX;
+    public float maxTranslateX;
+    int wayX;
+    float marginX;
+
+    private void Awake()
+    {
+        wayX = 1;
+        marginX = transform.position.x;
+    }
+
+    private void FixedUpdate()
+    {
+        if (transform.position.x > maxTranslateX + marginX)
+        {
+            wayX = -1;
+        }
+        else if(transform.position.x < marginX)
+            wayX = 1;
+        transform.position = new Vector3(transform.position.x + (speedTranslateX * Time.deltaTime) * wayX, 0, 0);
+    }
+}
